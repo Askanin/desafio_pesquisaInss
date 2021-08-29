e57407eea45dc0c48f6a2ed1ab4a7bacbb74e44e
Felipe Lima <flimas1985@gmail.com>
2021-08-29 00:04:29 -0300

    First commit

---
+++ src/main/java/com/bempromotora/PesquisaInssApplication.java
@@ -0,0 +1,13 @@
+package com.bempromotora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisaInssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisaInssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bempromotora/PesquisaInssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bempromotora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisaInssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
